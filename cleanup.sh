0e93a0a60a385dd98497b962e8e35d9c7f08196b
cdalitz <christoph.dalitz@hs-niederrhein.de>
2024-05-06 15:39:46 +0200

    cleanup script added

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,11 @@
 # Shell script for removal of old generated incipits
 #
 
-INCIPITDIR=www/mss/incipits
+#INCIPITDIR=www/mss/incipits
+INCIPITDIR=/var/www/ms-lute/incipits
+
+# disk usage before
+echo -n "`date +'%D %H:%M'`: $INCIPITDIR `du -sh $INCIPITDIR | cut -f 1` ->"
 
 # remove files older than one day, but not safeguard file
 touch "$INCIPITDIR/do-not-remove"
@@ -12,3 +16,7 @@ find "$INCIPITDIR" -type f -mtime +1 -delete
 
 # remove empty directories
 find "$INCIPITDIR" -type d -empty -delete
+
+# disk usage after removal
+du -sh "$INCIPITDIR" | cut -f 1
+
