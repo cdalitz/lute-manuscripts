d11eb057e8659324cb40bb1f5ddb56376da02dac
cdalitz <christoph.dalitz@hs-niederrhein.de>
2025-03-30 22:12:53 +0200

    fixed error in A-KR77

--- www-data/php_inc/persons.php
+++ www-data/php_inc/persons.php
@@ -1,5 +1,5 @@
 <?php
 ?>
-Magnus&nbsp;Andersson, Jo&nbsp;Bringmann, Richard&nbsp;Civiol, Jan&nbsp;&#268;i&#382;m&aacute;&#345;, Tim&nbsp;Crawford, Christoph&nbsp;Dalitz, Michael&nbsp;D&uuml;cker, Sarge Gerbode, François-Pierre&nbsp;Goy, Bernhard Fischer, Franco Fois, Wolfgang Hiller, Dieter&nbsp;Kirsch, Rainer Luckhardt, Florent Marie, Christian Meyer, Arthur&nbsp;Ness, Tristan&nbsp;von&nbsp;Neumann, André&nbsp;Nieuwlaat, Pietro&nbsp;Prosser, Peter&nbsp;Rauscher, John&nbsp;Robinson, Greet&nbsp;Schamp, Andreas&nbsp;Schlegel, Anna Wiktoria Swoboda, Michael&nbsp;Treder, Arto&nbsp;Wikla.
+Magnus&nbsp;Andersson, Jo&nbsp;Bringmann, Richard&nbsp;Civiol, Jan&nbsp;&#268;i&#382;m&aacute;&#345;, Tim&nbsp;Crawford, Christoph&nbsp;Dalitz, Michael&nbsp;D&uuml;cker, Sarge Gerbode, François-Pierre&nbsp;Goy, Bernhard Fischer, Franco Fois, Grzegorz Joachimiak, Wolfgang Hiller, Dieter&nbsp;Kirsch, Rainer Luckhardt, Florent Marie, Christian Meyer, Arthur&nbsp;Ness, Tristan&nbsp;von&nbsp;Neumann, André&nbsp;Nieuwlaat, Pietro&nbsp;Prosser, Peter&nbsp;Rauscher, John&nbsp;Robinson, Greet&nbsp;Schamp, Andreas&nbsp;Schlegel, Anna Wiktoria Swoboda, Michael&nbsp;Treder, Tom&nbsp;Van Eijgen, Arto&nbsp;Wikla.
 <?php
 ?>
